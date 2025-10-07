4085b95af8e46efb263430c48735207d0e4ad0d3
rhdzmota <code@rhdzmota.com>
2025-10-06 19:17:51 -0600

    OLD-1 Ajust given version refactor

--- opload/src/test/test_opload/test_version.py
+++ opload/src/test/test_opload/test_version.py
@@ -1,10 +1,9 @@
 from fred.version import Version
 
-from opload.utils import get_current_version
+from opload.version import version
 
 
 def test_get_current_version():
-    version = get_current_version()
     assert isinstance(version, Version)
     assert isinstance(version.major, int)
     assert isinstance(version.minor, int)
